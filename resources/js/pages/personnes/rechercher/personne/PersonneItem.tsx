34658a425bc206b8d654dfa05a844a50f7b5dd44
zdiawara <zakaridiadiawara@gmail.com>
2025-09-21 14:49:17 +0200

    fix affichage photo

--- resources/js/pages/personnes/rechercher/personne/PersonneItem.tsx
+++ resources/js/pages/personnes/rechercher/personne/PersonneItem.tsx
@@ -42,6 +42,7 @@ export const PersonneItem: FC<PersonneItemProps> = ({ personne }) => {
             style={{
               width: "100%",
               height: "100%",
+              minWidth: "50px",
               textAlign: "center",
               objectFit: "cover",
               color: "transparent",

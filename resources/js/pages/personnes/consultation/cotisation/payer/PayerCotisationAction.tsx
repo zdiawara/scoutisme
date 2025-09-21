e70079972e869253f0a427d7e1c979ade75c4613
zdiawara <zakaridiadiawara@gmail.com>
2025-09-21 09:14:37 +0200

    feat: ajouter contraintes sur paiement

--- resources/js/pages/personnes/consultation/cotisation/payer/PayerCotisationAction.tsx
+++ resources/js/pages/personnes/consultation/cotisation/payer/PayerCotisationAction.tsx
@@ -1,5 +1,5 @@
 import { FC, useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Modal } from "react-bootstrap";
 import { PersonneResource } from "types/personne.type";
 
 // import { useDroits } from "hooks/useDroits";
@@ -8,38 +8,46 @@ import { CreerPaiementModal } from "pages/paiements/modal";
 type PayerCotisationActionProps = {
   personne: PersonneResource;
   annee: string;
+  resteAPayer: number;
 };
 
-export const PayerCotisationAction: FC<PayerCotisationActionProps> = ({
-  personne,
-  annee,
-}) => {
+export const PayerCotisationAction: FC<PayerCotisationActionProps> = ({ personne, annee, resteAPayer }) => {
   const [action, setAction] = useState<string | undefined>();
   const closeModal = () => {
     setAction(undefined);
   };
 
   const onSelect = (code: string) => () => {
-    setAction(code);
+    if (resteAPayer <= 0) {
+      setAction("cotisation_a_jour");
+    } else {
+      setAction(code);
+    }
   };
 
   return (
     <>
-      <Button
-        size="sm"
-        className="ms-1"
-        onClick={onSelect("payer")}
-        variant="secondary"
-      >
+      <Button size="sm" className="ms-1" onClick={onSelect("payer")} variant="secondary">
         Payer
       </Button>
 
       {action === "payer" && (
-        <CreerPaiementModal
-          closeModal={closeModal}
-          personne={personne}
-          annee={annee}
-        />
+        <CreerPaiementModal resteAPayer={resteAPayer} closeModal={closeModal} personne={personne} annee={annee} />
+      )}
+
+      {action === "cotisation_a_jour" && (
+        <Modal show={true} onHide={closeModal} size="sm" centered animation>
+          <Modal.Body className="p-4">
+            <div className="text-center">
+              <i className="dripicons-checkmark h1"></i>
+              <h4 className="mt-2 text-primary">Information</h4>
+              <div className="mt-3 mb-3">Il ne reste aucune somme à payer.</div>
+              <Button variant="outline-primary" className="shadow-sm" onClick={closeModal}>
+                OK
+              </Button>
+            </div>
+          </Modal.Body>
+        </Modal>
       )}
     </>
   );

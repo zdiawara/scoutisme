da4a24ba2a654595e5c81616ff89da25b2871dbc
zdiawara <zakaridiadiawara@gmail.com>
2025-09-28 15:23:37 +0200

    fix mettre mail de verification en FR

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,11 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Lang;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +25,14 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // ...
+        VerifyEmail::toMailUsing(function (object $notifiable, string $verificationUrl) {
+            return (new MailMessage)
+                ->subject(Lang::get('Bienvenue ! Veuillez vérifier votre adresse email')) // Sujet personnalisé
+                ->greeting(Lang::get('Bonjour !'))
+                ->line(Lang::get('Veuillez cliquer sur le bouton ci-dessous pour vérifier votre adresse email.'))
+                ->action(Lang::get('Vérifier mon Email'), $verificationUrl)
+                ->line(Lang::get('Si vous n\'avez pas créé de compte, vous pouvez ignorer cet email.'));
+        });
     }
 }

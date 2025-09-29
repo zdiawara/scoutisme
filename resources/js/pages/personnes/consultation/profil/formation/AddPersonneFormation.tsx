072a9a9b1aacaf2f4302a6ec0f70e957c6152f80
zdiawara <zakaridiadiawara@gmail.com>
2025-09-29 10:09:41 +0200

    fix creation de formation

--- resources/js/pages/personnes/consultation/profil/formation/AddPersonneFormation.tsx
+++ resources/js/pages/personnes/consultation/profil/formation/AddPersonneFormation.tsx
@@ -1,16 +1,15 @@
 import { FC } from "react";
 import { ListGroup } from "react-bootstrap";
 import { PersonneFormationForm } from "./PersonneFormationForm";
+import { SelectItem } from "types/form.type";
+import { PersonneResource } from "types/personne.type";
 
 type AddPersonneFormationProps = {
   onClose: () => void;
-  onSave: (data: Record<string, any>) => Promise<any>;
+  onSave: (data: Record<string, Date | SelectItem>) => Promise<PersonneResource>;
 };
 
-export const AddPersonneFormation: FC<AddPersonneFormationProps> = ({
-  onClose,
-  onSave,
-}) => {
+export const AddPersonneFormation: FC<AddPersonneFormationProps> = ({ onClose, onSave }) => {
   return (
     <ListGroup.Item>
       <PersonneFormationForm onSave={onSave} goBack={onClose} />

d36864142c8a9f822ae23bd5884a0577e6e96cb4
zdiawara <zakaridiadiawara@gmail.com>
2025-10-07 10:37:35 +0200

    fix message retour

--- app/Exceptions/BadRequestException.php
+++ app/Exceptions/BadRequestException.php
@@ -6,5 +6,10 @@ use Exception;
 
 class BadRequestException extends Exception
 {
-    //
+    public function render($request)
+    {
+        return response()->json([
+            'message' => $this->getMessage() ?: 'Requête invalide.'
+        ], 400);
+    }
 }
